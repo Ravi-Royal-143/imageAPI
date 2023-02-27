47822b0c0b413fe750312905320a8c271db76754
Ravikumar C S <raviroyalmech233@gmail.com>
2023-02-27 17:03:20 +0530

    img copy to clipboard UI dist updated

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const cors = require('cors');
 const server = http.createServer(app);
 const compression = require('compression')
 const path = require("path");
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const upload = require('./multer');
 
+app.use(cors());
 app.use(compression());
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -22,9 +26,41 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(bodyParser.json());
+app.use("/file", express.static(path.join("file")));
 app.use("/", express.static(path.join(__dirname, "dist")));
 
-app.use(cors());
+app.post('/upload', upload.single('image'), (req, res) => {
+    const file = req.file;
+    if (!file) {
+      return res.status(400).send('No file uploaded');
+    }
+    fs.writeFile(`file/${file.originalname}`, file.buffer, (err) => {
+      if (err) {
+        return res.status(500).send('Error saving file');
+      }
+      res.json({msg: 'File saved successfully'});
+    });
+});
+
+const dirnameExportImg = './file'
+app.get('/list', (req, res) => {
+    fs.readdir(dirnameExportImg, function (err, files) {
+        if (err) {
+            console.log(err)
+            res.status(500).json({
+                message: "Fetch Images failed!",
+                error: err
+            });
+        }
+        res.send(files);
+    })
+});
+
+app.delete('/delete/:path', (req, res) => {
+    fs.unlinkSync('file/'+req.params.path);
+    res.send({ message: 'Delete successfully'});
+})
 
 app.get('/test', (req, res) => {
     res.send('test');
